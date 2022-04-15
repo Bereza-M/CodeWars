7ed1a055143a7cfbca600302d04c5d00ebd5a637
Bereza-M <bereznev@tut.by>
2022-04-16 01:23:10 +0300

    16.04

--- checkSameCase.js
+++ checkSameCase.js
@@ -1,4 +1,18 @@
 function sameCase(a, b){
-    if (charCodeAt(a)>=65 && charCodeAt(b)>=65 && charCodeAt(a)<=90 && charCodeAt(b)<=90){
+    if (a.charCodeAt()>=65 && b.charCodeAt()>=65 && a.charCodeAt()<=90 && b.charCodeAt()<=90){
       return 1;  
-    };
+    }
+    else if (a.charCodeAt()>=97 && b.charCodeAt()>=97 && a.charCodeAt()<=122 && b.charCodeAt()<=122){
+      return 1;  
+    }
+    else if ((a.charCodeAt()>=65 && a.charCodeAt()<=90) && (b.charCodeAt()>=97 && b.charCodeAt()<=122)){
+        return 0;
+    }
+    else if ((b.charCodeAt()>=65 && b.charCodeAt()<=90) && (a.charCodeAt()>=97 && a.charCodeAt()<=122)){
+        return 0;
+    }
+    
+    else {
+    return -1;
+    }
+}
