893569822d41583848c7967b15b526913e6c3055
Bereza-M <bereznev@tut.by>
2022-04-22 23:48:09 +0300

    22.04

--- areaOfASquare.js
+++ areaOfASquare.js
@@ -2,3 +2,8 @@ function squareArea(A){
     let square = Math.pow(2 * A/Math.PI, 2);
     return Math.floor(square * 100) / 100;
   }
+
+function squareArea(A){
+    let square = Math.pow(2 * A/Math.PI, 2);
+    return Math.floor(square * 100) / 100;
+}
